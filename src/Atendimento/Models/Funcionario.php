9a77624069a80b17a02a2e49f0196d15c5ade7ee
manzoli2122 <manzoli2122@gmail.com>
2017-12-07 18:28:20 -0200

    inicial

--- src/Atendimento/Models/Funcionario.php
+++ src/Atendimento/Models/Funcionario.php
@@ -12,47 +12,14 @@ class Funcionario extends Model
         $this->table = Config::get('atendimento.funcionario_table' , 'users') ;    
     }
 
-   
-   
+      
     protected $fillable = [
-        'name', 'email',   'apelido' , 'nascimento' , 'celular' , 'ativo'
+        'name', 'email',   'apelido' , 'ativo'
     ];
-
-   
-
-
-    protected $dates = [
-        'created_at',
-        'updated_at',
-        'nascimento'
-    ];
-
-
-
-    public function scopeAtivo($query)
-    {
-        return $query->where('ativo', 1)->whereIn('id', function($query2) { 
-                                                        $query2->select("perfils_users.user_id");
-                                                        $query2->from("perfils_users");
-                                                        $query2->whereIn("perfils_users.perfil_id" , function($query3) {
-                                                            $query3->select("perfils.id");
-                                                            $query3->from("perfils");
-                                                            $query3->where('nome' , 'Funcionario');
-                                                        } );                                                            
-                                                    });
-    }
-
-    
-
-    public function index($totalPage)
-    {
-        return $this->ativo()->orderBy('name', 'asc')->paginate($totalPage);        
-    }
-
 
     
     public static function funcionarios(){       
-        return  Funcionario::whereIn('id', function($query2) { //} use ($user){
+        return  Funcionario::where('ativo', 1)->whereIn('id', function($query2) { //} use ($user){
                         $query2->select("perfils_users.user_id");
                         $query2->from("perfils_users");
                         $query2->whereIn("perfils_users.perfil_id" , function($query3) {
@@ -62,32 +29,6 @@ class Funcionario extends Model
                         } );                                                            
             })->get();         
     }
-
-
-    public function Atendimentos()
-    {
-        return $this->hasMany('Manzoli2122\Salao\Atendimento\Models\AtendimentoFuncionario', 'funcionario_id');
-       
-    }
-
-
-
-    public function AtendimentosSemSalario()
-    {
-        return $this->hasMany('Manzoli2122\Salao\Atendimento\Models\AtendimentoFuncionario', 'funcionario_id')->whereNull('salario_id')->get();
-        //return $this->whereNull('salario_id')->where('funcionario_id' , $funcionarioId)->get();
-    }
-
-
-
-
-
-
-
-
-
-
-
 
 
 
