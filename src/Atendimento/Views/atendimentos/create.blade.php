1e96c8aa641b5591b2da62e9fc6f6af64358cdfb
Bruno Manzoli do Nascimento <manzoli2122@gmail.com>
2018-01-29 18:28:48 -0200

    012-01-2018

--- src/Atendimento/Views/atendimentos/create.blade.php
+++ src/Atendimento/Views/atendimentos/create.blade.php
@@ -120,8 +120,7 @@
                         </div>
                     </div>                    
                 </div>                	
-            @empty
-			
+            @empty			
 			@endforelse 
              <div class="row">        
                 <div class="col-md-12">
@@ -131,16 +130,13 @@
                 <div class="col-md-12">
                     <h3 style="text-align:right; margin:0px; margin-top:25px; color:red;"> Dividas atrasadas R$ {{number_format($atendimento->servicoAnterioresFiados(), 2 ,',', '') }} </h3>
                 </div>  
-
                 <div class="col-md-12">
                     <h3 style="text-align:right; margin:0px; margin-top:25px;"> Valor Total R$ {{number_format($atendimento->valor, 2 ,',', '') }} </h3>
                 </div>
-
                 <div class="col-md-12">
                     <p style="margin-bottom:20px; margin-top:10px">
                         <form class="form form-search form-ds" method="post" action="{{route('atendimentos.finalizar', $atendimento->id)}}" onsubmit="return  finalizarSend(this)">
-                            {{csrf_field()}}
-                        
+                            {{csrf_field()}}                        
                             <input name="total_pagamento" value="{{$atendimento->valor}}" type="hidden">
                             <input name="total_pagamento" value="{{$atendimento->valorPagamentos()}}" type="hidden">
                             <button type="submit" class="btn btn-success" style="width: 100%;" >
@@ -149,45 +145,33 @@
                         </form>
                     </p>
                 </div>
-
-
                 <div class="col-md-12">
                     <a style="width: 100%;" class="btn btn-warning" href='{{route("atendimentos.cancelar", $atendimento->id)}}'>
-                    <i class="fa fa-delete" aria-hidden="true"></i>
-                    Cancelar
-                </a>      
+                        <i class="fa fa-delete" aria-hidden="true"></i>
+                        Cancelar
+                    </a>      
                 </div>
-
-
             </div>     
-
             <div class="row">        
-                <div class="col-md-12">
-                   
+                <div class="col-md-12">                   
                 </div>                    
             </div>   
         </div>            
     </section>
 
-
-@include('atendimento::atendimentos.servicoModal')                  
-@include('atendimento::atendimentos.produtoModal')                
-@include('atendimento::atendimentos.pagamentoModal')
-
+    @include('atendimento::atendimentos.servicoModal')                  
+    @include('atendimento::atendimentos.produtoModal')                
+    @include('atendimento::atendimentos.pagamentoModal')
 
 @endsection
-
-
   
 @push( Config::get('app.templateMasterScript' , 'script')  )
         <script src="{{url('/js/app.js')}}"></script>	
         <script>
-            function finalizarSend(val) {
-                
+            function finalizarSend(val) {                
                 var atendimento = val.elements['total_atendimento'].value
                 var pagamento = val.elements['total_pagamento'].value
-                var dif = atendimento - pagamento ;
-                
+                var dif = atendimento - pagamento ;                
                 if(dif > 0.09) {
                     alert('O valor total do atendimento que é R$' + atendimento + 
                     ' não confere com o do pagamento que é R$' + pagamento  );
@@ -196,35 +180,23 @@
                 return true;
             }
 
-        
-
-            function produtoFunction(form) {
-                                    
-                var max = parseFloat( form.elements['produto_id'].options[form.elements['produto_id'].selectedIndex].text );
-            
+            function produtoFunction(form) {                                    
+                var max = parseFloat( form.elements['produto_id'].options[form.elements['produto_id'].selectedIndex].text );            
                 var quantidade = parseInt(form.elements['quantidade'].value);
-
                 var desconto_maximo =  parseInt(form.elements['produto_id'].options[form.elements['produto_id'].selectedIndex].dataset['maximo']);
-
                 form.elements['desconto'].max = ( desconto_maximo * max / 100); 
-
                 if( max != 0.0 ){
                     form.elements['acrescimo'].max = max ;   
-                }
-            
-
+                }         
 
                 if(form.elements['desconto'].value == '')
                     form.elements['desconto'].value = 0.0;
-                var desconto =  parseFloat( form.elements['desconto'].value) ;
-                
+                var desconto =  parseFloat( form.elements['desconto'].value) ;            
                 
                 if(form.elements['acrescimo'].value == '')
                     form.elements['acrescimo'].value = 0.0;
                 var acrescimo = parseFloat( form.elements['acrescimo'].value );
 
-            
-            
                 var valor_unitario = max - desconto + acrescimo ;  
 
                 var valor_total = valor_unitario * quantidade;           
@@ -235,54 +207,33 @@
 
             }
 
-
-
             function servicoFunction(form) {
-
-                var max = parseFloat( form.elements['servico_id'].options[form.elements['servico_id'].selectedIndex].text );
-            
+                var max = parseFloat( form.elements['servico_id'].options[form.elements['servico_id'].selectedIndex].text );            
                 var quantidade = parseInt(form.elements['quantidade'].value);
-
                 //alert(quantidade);
                 var desconto_maximo =  parseInt(form.elements['servico_id'].options[form.elements['servico_id'].selectedIndex].dataset['maximo']);
-
                 form.elements['desconto'].max = ( desconto_maximo * max / 100); 
-
                 form.elements['acrescimo'].max = max ;   
-
                 if(form.elements['desconto'].value == '')
                     form.elements['desconto'].value = 0.0;
-                var desconto =  parseFloat( form.elements['desconto'].value) ;
-            
-            
+                var desconto =  parseFloat( form.elements['desconto'].value) ;            
                 if(form.elements['acrescimo'].value == '')
                     form.elements['acrescimo'].value = 0.0;
                 var acrescimo = parseFloat( form.elements['acrescimo'].value );
-                
                 var valor_unitario = max - desconto + acrescimo ;  
-
                 var valor_total = valor_unitario * quantidade;           
-
                 form.elements['valor-produto-unitario'].value = valor_unitario;
-
                 form.elements['valor-produto-total'].value = valor_total;
-
                 //var max = val.options[val.selectedIndex].text;
                 //val.form.elements['desconto'].max = (4 * max / 5 ); 
             }
 
-
-
-
-
-            function myFunction(val) {
-                            
+            function myFunction(val) {                            
                 if(val == 'credito'){
                                 document.getElementById("form-operadora").hidden = false ;
                                 document.getElementById("form-parcelas").hidden = false ;
                                 document.getElementById("parcelas").selectedIndex = 1 ;
                                 document.getElementById("form-bandeira").hidden = false ;
-                                
                                 document.getElementById("operadora_id").required = true ;
                                 document.getElementById("parcelas").required = true ;
                                 document.getElementById("bandeira").required = true ;
@@ -292,7 +243,6 @@
                                 document.getElementById("form-operadora").hidden = false ;
                                 document.getElementById("form-parcelas").hidden = true ;
                                 document.getElementById("form-bandeira").hidden = false ;
-
                                 document.getElementById("operadora_id").required = true ;
                                 document.getElementById("parcelas").required = false ;
                                 document.getElementById("bandeira").required = true ;
@@ -301,7 +251,6 @@
                                 document.getElementById("form-operadora").hidden = true ;
                                 document.getElementById("form-parcelas").hidden = true ;
                                 document.getElementById("form-bandeira").hidden = true ;
-
                                 document.getElementById("operadora_id").required = false ;
                                 document.getElementById("parcelas").required = false ;
                                 document.getElementById("bandeira").required = false ;
@@ -341,11 +290,7 @@
                                 document.getElementById("operadora_id").required = false ;
                                 document.getElementById("parcelas").required = false ;
                                 document.getElementById("bandeira").required = false ;
-                }
-
-
-            
+                }            
             }	
         </script>	
 @endpush
-
--- src/Atendimento/Views/atendimentos/index.blade.php
+++ src/Atendimento/Views/atendimentos/index.blade.php
@@ -1,61 +1,49 @@
 @extends( Config::get('app.templateMaster' , 'templates.templateMaster')  )
 	
 @section( Config::get('app.templateMasterContentTitulo' , 'titulo-page')  )
-			Atendimentos do dia {{ today()->format('d/m/Y')}} 
+	Atendimentos do dia {{ today()->format('d/m/Y')}} 
 @endsection
-
 
 @section( Config::get('app.templateMasterContent' , 'content')  )
  	
-			
-				<div class="col-xs-12">
-					<div class="box">						
-						
-						<div class="box-body table-responsive no-padding">
-							<table class="table table-hover table-striped">
-								<tr>
-									<th>Cliente</th>
-									<th>Valor</th>				
-									<th>Ações</th>
-								</tr>
-								@forelse($models as $model)				
-									<tr>
-										<td> {{ $model->cliente->name }}  </td>						
-										<td> R$ {{number_format($model->valor, 2 , ',' , '' )}} </td>
-										<td>
-												@permissao('atendimentos')								
-														<a class="btn btn-success btn-sm" href='{{route("atendimentos.show", $model->id)}}'>
-															<i class="fa fa-eye" aria-hidden="true"></i>Exibir</a>	
-														 <a class="btn btn-warning btn-sm" data-toggle="modal" data-target="#alterarDataModal{{$model->id}}" > 
-															Alterar Data
-														</a>							
-												@endpermissao																
+	<div class="col-xs-12">
+		<div class="box">									
+			<div class="box-body table-responsive no-padding">
+				<table class="table table-hover table-striped">
+					<tr>
+						<th>Cliente</th>
+						<th>Valor</th>				
+						<th>Ações</th>
+					</tr>
+					@forelse($models as $model)				
+					<tr>
+						<td> {{ $model->cliente->name }}  </td>						
+							<td> R$ {{number_format($model->valor, 2 , ',' , '' )}} </td>
+						<td>
+							@permissao('atendimentos')								
+								<a class="btn btn-success btn-sm" href='{{route("atendimentos.show", $model->id)}}'>
+									<i class="fa fa-eye" aria-hidden="true"></i>Exibir</a>	
+								 <a class="btn btn-warning btn-sm" data-toggle="modal" data-target="#alterarDataModal{{$model->id}}" > 
+									Alterar Data
+								</a>							
+							@endpermissao																
 											
-												@permissao('atendimentos-soft-delete')			
-													<a class="btn btn-danger btn-sm"  href="javascript:void(0);" onclick="$(this).find('form').submit();" >
-															{!! Form::open(['route' => ['atendimentos.destroySoft', $model->id ],  'method' => 'DELETE' , 'onsubmit' => " return  ApagarAtendimento(this)" ])!!}                                        
-															{!! Form::close()!!}    
-															<i class="fa fa-trash" aria-hidden="true"></i>Apagar</a>													
-												@endpermissao
-											
-										</td>
-									</tr>
-								@empty
-									
-								@endforelse
-								
-						
-
-							</table>
-					</div>
-
-
-					
-					<!-- /.box-body -->
-				</div>
-				<!-- /.box -->
-				</div>
-
+							@permissao('atendimentos-soft-delete')			
+								<a class="btn btn-danger btn-sm"  href="javascript:void(0);" onclick="$(this).find('form').submit();" >
+									<form  method="post" action="{{route('atendimentos.destroySoft', $model->id)}}" onsubmit="return  ApagarAtendimento(this)">
+										{{csrf_field()}}    
+										<input name="_method" value="DELETE" type="hidden">                    
+									</form>  
+									<i class="fa fa-trash" aria-hidden="true"></i>Apagar</a>													
+							@endpermissao											
+						</td>
+					</tr>
+					@empty					
+					@endforelse					
+				</table>
+			</div>					<!-- /.box-body -->
+		</div>				<!-- /.box -->
+	</div>
 
 	@forelse($models as $model)	
 			@include('atendimento::atendimentos.modalAterarData')
@@ -63,8 +51,6 @@
 	@endforelse
 
 @endsection
-
-
 		
 @push( Config::get('app.templateMasterScript' , 'script')  )
         	<script>$(function(){setTimeout("$('.hide-msg').fadeOut();",5000)});</script>
