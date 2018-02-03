83d9bb864661dd29f3c216a8a5c251414020d8e5
Bruno Manzoli do Nascimento <manzoli2122@gmail.com>
2018-02-03 08:23:30 -0200

    30-01-2018

--- src/Atendimento/Models/Pagamento.php
+++ src/Atendimento/Models/Pagamento.php
@@ -72,7 +72,7 @@ class Pagamento extends Model
     public function getDatatable()
     {
         return DB::table('pagamentos_view')->whereNull('deleted_at') //->where('operadora_confirm', false )
-         ->select(['id', 'cliente', 'created_at' ,  'valor' , 'formaPagamento', 'bandeira' , 'nome' , 'operadora_confirm' , 'operado', 'na_conta' ]); 
+         ->select(['id', 'cliente', 'created_at' ,  'valor' , 'formaPagamento', 'bandeira' , 'nome' , 'operadora_confirm' , 'operado', 'caiu_conta'. 'na_conta' ]); 
         //return $this->where('operadora_confirm', false )->select(['id',   DB::raw(  " date_format( created_at , '%d-%m-%Y' ) as created_atd" )   ,  'operadora_confirm' , 'formaPagamento' , 
                         //DB::raw(  " concat('R$', ROUND  (valor , 2 ) ) as valor" )   ]);        
     }   
