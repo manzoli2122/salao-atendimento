9635162a59cc0635407a9a9eedd823225176ee46
Bruno Manzoli do Nascimento <manzoli2122@gmail.com>
2018-02-08 18:35:36 -0200

    30-01-2018

--- src/Atendimento/Views/atendimentos/create.blade.php
+++ src/Atendimento/Views/atendimentos/create.blade.php
@@ -219,7 +219,7 @@
                                 item: ui.item.option
                             });
                         },  
-                        autocompletechange: "_removeIfInvalid" , "_servicoFunction"
+                        autocompletechange: "_removeIfInvalid"  
                     });
                 },
   
@@ -267,10 +267,8 @@
                 },
 
                 _servicoFunction: function( event, ui ) {
-                    var form = document.forms["form-servico"] ;
-                
-                    var servico =  form.elements['servico-select'].options[form.elements['servico-select'].selectedIndex] ;
-
+                    var form = document.forms["form-servico"] ;                
+                    var servico =  form["servico_id"].options[form["servico_id"].selectedIndex] ;
                     //document.getElementById('div-form-servico-servico').getElementsByClassName("es-visible selected")[0] ;                 
                     var quantidade = parseInt( form["quantidade"].value );                
                     var desconto_maximo = parseInt(  servico.dataset.maximo );                
@@ -295,6 +293,7 @@
                 _removeIfInvalid: function( event, ui ) {
                     // Selected an item, nothing to do
                     if ( ui.item ) {
+                        this._servicoFunction();
                         return;
                     } 
                     // Search for a match (case-insensitive)
@@ -309,6 +308,7 @@
                     });  
                     // Found a match, nothing to do
                     if ( valid ) {
+                        this._servicoFunction();
                         return;
                     }  
                     // Remove invalid value
@@ -330,10 +330,7 @@
             });
   
             $( "#servico_id" ).combobox();
-            $( "#funcionario_id" ).combobox();
-            //$( "#toggle" ).on( "click", function() {
-            // $( "#categoria" ).toggle();
-            //});
+            
         } );
      </script>
 
@@ -412,7 +409,7 @@
  
                 var form = document.forms["form-servico"] ;
                 
-                var servico =  form.elements['servico-select'].options[form.elements['servico-select'].selectedIndex] ;
+                var servico =  form["servico_id"].options[form["servico_id"].selectedIndex] ;
 
                 //document.getElementById('div-form-servico-servico').getElementsByClassName("es-visible selected")[0] ;                 
                 var quantidade = parseInt( form["quantidade"].value );                
@@ -565,6 +562,159 @@
             }	
             
         </script>	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+
+    <script>
+        $( function() {
+            $.widget( "custom.combobox", {
+                
+                _create: function() {
+                    this.wrapper = $( "<span>" )
+                        .addClass( "custom-combobox" )
+                        .insertAfter( this.element );  
+                    this.element.hide();
+                    this._createAutocomplete();
+                    this._createShowAllButton();
+                },
+  
+                _createAutocomplete: function() {
+                    var selected = this.element.children( ":selected" ),
+                    value = selected.val() ? selected.text() : "";
+                    this.input = $( "<input>" )
+                        .appendTo( this.wrapper )
+                        .val( value )
+                        .attr( "title", "" )
+                        .attr( "style", "width: 90%;     display: inline;" )
+                        .addClass( "custom-combobox-input ui-widget ui-widget-content ui-state-default ui-corner-left form-control" )
+                        .autocomplete({
+                            delay: 0,
+                            minLength: 0,
+                            source: $.proxy( this, "_source" )
+                        })
+                        .tooltip({
+                            classes: {
+                                "ui-tooltip": "ui-state-highlight"
+                            }
+                        });
+  
+                    this._on( this.input, {
+                        autocompleteselect: function( event, ui ) {
+                            ui.item.option.selected = true;
+                            this._trigger( "select", event, {
+                                item: ui.item.option
+                            });
+                        },  
+                        autocompletechange: "_removeIfInvalid"  
+                    });
+                },
+  
+                _createShowAllButton: function() {
+                    var input = this.input,
+                        wasOpen = false;
+                    $( "<a>" )
+                        .attr( "tabIndex", -1 )
+                        .attr( "title", "Show All Items" )
+                        .tooltip()
+                        .appendTo( this.wrapper )
+                        .button({
+                            icons: {
+                                primary: "ui-icon-triangle-1-s"
+                            },
+                            text: false
+                        })
+                        .removeClass( "ui-corner-all" )
+                        .addClass( "custom-combobox-toggle ui-corner-right form-control" )
+                        .on( "mousedown", function() {
+                             wasOpen = input.autocomplete( "widget" ).is( ":visible" );
+                        })
+                        .on( "click", function() {
+                            input.trigger( "focus" );  
+                            // Close if already visible
+                            if ( wasOpen ) {
+                                return;
+                            }  
+                            // Pass empty string as value to search for, displaying all results
+                            input.autocomplete( "search", "" );
+                        });
+                },
+  
+                _source: function( request, response ) {
+                    var matcher = new RegExp( $.ui.autocomplete.escapeRegex(request.term), "i" );
+                    response( this.element.children( "option" ).map(function() {
+                        var text = $( this ).text();
+                        if ( this.value && ( !request.term || matcher.test(text) ) )
+                            return {
+                                label: text,
+                                value: text,
+                                option: this
+                            };
+                    }) );
+                },
+
+        
+
+                _removeIfInvalid: function( event, ui ) {
+                    // Selected an item, nothing to do
+                    if ( ui.item ) {
+                        return;
+                    } 
+                    // Search for a match (case-insensitive)
+                    var value = this.input.val(),
+                    valueLowerCase = value.toLowerCase(),
+                    valid = false;
+                    this.element.children( "option" ).each(function() {
+                        if ( $( this ).text().toLowerCase() === valueLowerCase ) {
+                            this.selected = valid = true;
+                            return false;
+                        }
+                    });  
+                    // Found a match, nothing to do
+                    if ( valid ) {
+                        return;
+                    }  
+                    // Remove invalid value
+                    this.input
+                        .val( "" )
+                        .attr( "title", value + " didn't match any item" )
+                        .tooltip( "open" );
+                    this.element.val( "" );
+                    this._delay(function() {
+                        this.input.tooltip( "close" ).attr( "title", "" );
+                    }, 2500 );
+                    this.input.autocomplete( "instance" ).term = "";
+                },
+  
+                _destroy: function() {
+                    this.wrapper.remove();
+                    this.element.show();
+                }
+            });
+  
+           
+            $( "#funcionario_id" ).combobox();
+            
+        } );
+     </script>
+
+
+
 
 
 
