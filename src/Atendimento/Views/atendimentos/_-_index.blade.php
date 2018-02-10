32ee8d18a7a63cf6d7e4e99f09f84320ebaf9bdb
Bruno Manzoli do Nascimento <manzoli2122@gmail.com>
2018-02-10 14:44:13 -0200

    30-01-2018

--- src/Atendimento/Views/atendimentos/_-_index.blade.php
+++ src/Atendimento/Views/atendimentos/_-_index.blade.php
@@ -6,7 +6,53 @@
 
 @section( Config::get('app.templateMasterContent' , 'content')  )
  	
-	
+	<!--div class="col-xs-12">
+		<div class="box">									
+			<div class="box-body table-responsive no-padding"-->
+
+	<!--div class="col-xs-12">
+		<div class="box box-success">	
+			<div class="box-body">
+
+				<table class="table table-hover table-striped table-hover table-responsive">
+					<tr>
+						<th>Cliente</th>
+						<th>Valor dos Serviços</th>
+						<th>Valor dos Produtos</th>	
+						<th>Valor Total</th>					
+						<th>Ações</th>
+					</tr>
+					@forelse($models as $model)				
+					<tr>
+						<td> {{ $model->cliente->name }}  </td>			
+						<td> R$ {{number_format($model->valorServicos(), 2 , ',' , '' )}} </td>
+						<td> R$ {{number_format($model->valorProdutos(), 2 , ',' , '' )}} </td>			
+						<td> R$ {{number_format($model->valor, 2 , ',' , '' )}} </td>
+						<td>
+							@permissao('atendimentos')								
+								<a class="btn btn-success btn-sm" href='{{route("atendimentos.show", $model->id)}}'>
+									<i class="fa fa-eye" aria-hidden="true"></i>Exibir</a>	
+								 <a class="btn btn-warning btn-sm" data-toggle="modal" data-target="#alterarDataModal{{$model->id}}" > 
+									Alterar Data
+								</a>							
+							@endpermissao																
+											
+							@permissao('atendimentos-soft-delete')			
+								<a class="btn btn-danger btn-sm"  href="javascript:void(0);" onclick="$(this).find('form').submit();" >
+									<form  method="post" action="{{route('atendimentos.destroySoft', $model->id)}}" onsubmit="return  ApagarAtendimento(this)">
+										{{csrf_field()}}    
+										<input name="_method" value="DELETE" type="hidden">                    
+									</form>  
+									<i class="fa fa-trash" aria-hidden="true"></i>Apagar</a>													
+							@endpermissao											
+						</td>
+					</tr>
+					@empty					
+					@endforelse					
+				</table>
+			</div>					
+		</div>				
+	</div-->
 	@forelse($models as $model)	
 			@include('atendimento::atendimentos.modalAterarData')
 	@empty									
@@ -14,6 +60,7 @@
 
 
 </div>
+<!--h2 class="page-header">AdminLTE Custom Tabs</h2-->
 <div class="row">
 
 <div class="col-md-12">
@@ -25,7 +72,7 @@
 				<li><a href="#funcionario_{{$key->id}}" data-toggle="tab"> {{ $key->apelido }}</a></li>
 			@endforeach			
 
-          
+            <!--li><a href="#tab_2" data-toggle="tab">Serviços Realizados</a></li-->
             
         </ul>
         <div class="tab-content">
@@ -95,6 +142,41 @@
 			</div>
 			@endforeach
 
+
+
+
+
+
+
+
+
+
+
+            <!-- /.tab-pane -->
+            <!--div class="tab-pane" id="tab_2">	
+					<table class="table table-hover table-striped table-hover table-responsive">
+							<tr>
+								<th>Cliente</th>
+								<th>Funcionário</th>
+								<th>Valor dos Produtos</th>	
+								<th>Valor Total</th>
+							</tr>
+							@forelse($models as $model1)							
+								@forelse($model1->servicos as $model)
+									<tr>
+										<td> {{ $model->cliente->name }}  </td>			
+										<td> {{ $model->funcionario->name }} </td>
+										<td> {{ $model->servico->nome }} </td>			
+										<td> R$ {{number_format($model->valor, 2 , ',' , '' )}} </td>								
+									</tr>
+								@empty					
+								@endforelse
+							
+							@empty					
+							@endforelse					
+						</table>
+			</div-->
+			
 
         <!-- /.tab-pane -->
         </div>
