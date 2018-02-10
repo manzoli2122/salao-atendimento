3cb25d2f12eb131731e826100c195cb00fe4dd9e
Bruno Manzoli do Nascimento <manzoli2122@gmail.com>
2018-02-10 11:09:03 -0200

    30-01-2018

--- src/Atendimento/Views/atendimentos/index.blade.php
+++ src/Atendimento/Views/atendimentos/index.blade.php
@@ -63,7 +63,7 @@
 <h2 class="page-header">AdminLTE Custom Tabs</h2>
 <div class="row">
 
-<div class="col-md-6">
+<div class="col-md-12">
     <!-- Custom Tabs -->
     <div class="nav-tabs-custom">
         <ul class="nav nav-tabs">
