31fc292b4481ad181e5dfac12a67e53bc16fee69
Bruno Manzoli do Nascimento <manzoli2122@gmail.com>
2018-02-11 07:31:34 -0200

    30-01-2018

--- src/Atendimento/Views/atendimentos/index.blade.php
+++ src/Atendimento/Views/atendimentos/index.blade.php
@@ -133,18 +133,18 @@
 								<div class="progress-bar" style="width: 50%"></div>
 							</div>
 							<span class="progress-description">
-								50% Increase in 30 Days
+								
 							</span>
 						</div>
 					</div>
 				</div>
 
 				<div class="col-md-3 col-sm-6 col-xs-12">
-						<div class="info-box bg-yellow">
-							<span class="info-box-icon"><i class="ion ion-ios-money-outline"></i></span>
+						<div class="info-box bg-red">
+							<span class="info-box-icon"><i class="fa fa-credit-card"></i></span>
 							<div class="info-box-content">
-								<span class="info-box-text">Inventory</span>
-								<span class="info-box-number">5,200</span>
+								<span class="info-box-text">TOTAL EM CREDITO:</span>
+								<span class="info-box-number">{{ $caixa->valor_Pagamento_credito() }}</span>
 								<div class="progress">
 									<div class="progress-bar" style="width: 50%"></div>
 								</div>
@@ -156,27 +156,27 @@
 					</div>
 
 					<div class="col-md-3 col-sm-6 col-xs-12">
-							<div class="info-box bg-yellow">
-								<span class="info-box-icon"><i class="ion ion-ios-pricetag-outline"></i></span>
+							<div class="info-box bg-green">
+								<span class="info-box-icon"><i class="fa fa-credit-card"></i></span>
 								<div class="info-box-content">
-									<span class="info-box-text">Inventory</span>
-									<span class="info-box-number">5,200</span>
+									<span class="info-box-text"> TOTAL EM DEBITO:</span>
+									<span class="info-box-number">{{ $caixa->valor_Pagamento_debito() }}</span>
 									<div class="progress">
 										<div class="progress-bar" style="width: 50%"></div>
 									</div>
 									<span class="progress-description">
-										50% Increase in 30 Days
+										
 									</span>
 								</div>
 							</div>
 						</div>
 
 						<div class="col-md-3 col-sm-6 col-xs-12">
-								<div class="info-box bg-yellow">
-									<span class="info-box-icon"><i class="ion ion-ios-pricetag-outline"></i></span>
+								<div class="info-box bg-purple">
+									<span class="info-box-icon"><i class="fa fa-paypal"></i></span>
 									<div class="info-box-content">
-										<span class="info-box-text">Inventory</span>
-										<span class="info-box-number">5,200</span>
+										<span class="info-box-text">TOTAL EM PIC PAY:</span>
+										<span class="info-box-number">{{ $caixa->valor_Pagamento_pic_pay() }}</span>
 										<div class="progress">
 											<div class="progress-bar" style="width: 50%"></div>
 										</div>
