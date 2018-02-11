05e3f713a0313503654e3898168e4c5b0e294d70
Bruno Manzoli do Nascimento <manzoli2122@gmail.com>
2018-02-11 06:59:01 -0200

    30-01-2018

--- src/Atendimento/Views/atendimentos/index.blade.php
+++ src/Atendimento/Views/atendimentos/index.blade.php
@@ -34,7 +34,7 @@
     <div class="nav-tabs-custom">
         <ul class="nav nav-tabs">
 			<li class="active"><a href="#tab_1" data-toggle="tab">ATENDIMENTOS</a></li>
-			<li class="active"><a href="#caixa" data-toggle="tab">CAIXA</a></li>
+			<li><a href="#caixa" data-toggle="tab">CAIXA</a></li>
 			@foreach (Manzoli2122\Salao\Atendimento\Models\Funcionario::funcionarios() as $key )
 				<li><a href="#funcionario_{{$key->id}}" data-toggle="tab"> {{ $key->apelido }}</a></li>
 			@endforeach	
@@ -79,7 +79,48 @@
 			</div>
 
 
-			<div class="tab-pane active" id="caixa">
+			<div class="tab-pane" id="caixa">
+
+				<div class="col-md-3 col-sm-6 col-xs-12">
+					<div class="info-box">
+						<span class="info-box-icon bg-aqua"><i class="ion ion-ios-gear-outline"></i></span>				  
+						<div class="info-box-content">
+							<span class="info-box-text">CPU Traffic</span>
+							<span class="info-box-number">90<small>%</small></span>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-3 col-sm-6 col-xs-12">
+					<div class="info-box">
+						<span class="info-box-icon bg-red"><i class="fa fa-google-plus"></i></span>
+				  		<div class="info-box-content">
+							<span class="info-box-text">Likes</span>
+							<span class="info-box-number">41,410</span>
+						</div>
+					</div>
+				</div>				 
+				<div class="clearfix visible-sm-block"></div>
+				<div class="col-md-3 col-sm-6 col-xs-12">
+					<div class="info-box">
+							<span class="info-box-icon bg-green"><i class="ion ion-ios-cart-outline"></i></span>
+						<div class="info-box-content">
+							<span class="info-box-text">Sales</span>
+							<span class="info-box-number">760</span>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-3 col-sm-6 col-xs-12">
+					<div class="info-box">
+						<span class="info-box-icon bg-yellow"><i class="ion ion-ios-people-outline"></i></span>
+				  		<div class="info-box-content">
+							<span class="info-box-text">New Members</span>
+							<span class="info-box-number">2,000</span>
+						</div>
+					</div>
+				</div>
+
+
+				
 				<table class="table table-hover table-striped">
 					<tr>
 						<th>Caixa</th>
