3cd00ff228960e6b6077075e281e84e1946c9127
Bruno Manzoli do Nascimento <manzoli2122@gmail.com>
2018-02-11 07:13:28 -0200

    30-01-2018

--- src/Atendimento/Views/atendimentos/index.blade.php
+++ src/Atendimento/Views/atendimentos/index.blade.php
@@ -120,7 +120,79 @@
 				</div>
 
 
+
+
+
+				<div class="col-md-3 col-sm-6 col-xs-12">
+					<div class="info-box bg-yellow">
+						<span class="info-box-icon"><i class="ion ion-ios-pricetag-outline"></i></span>
+						<div class="info-box-content">
+							<span class="info-box-text">Inventory</span>
+							<span class="info-box-number">5,200</span>
+							<div class="progress">
+								<div class="progress-bar" style="width: 50%"></div>
+							</div>
+							<span class="progress-description">
+								50% Increase in 30 Days
+							</span>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-md-3 col-sm-6 col-xs-12">
+						<div class="info-box bg-yellow">
+							<span class="info-box-icon"><i class="ion ion-ios-pricetag-outline"></i></span>
+							<div class="info-box-content">
+								<span class="info-box-text">Inventory</span>
+								<span class="info-box-number">5,200</span>
+								<div class="progress">
+									<div class="progress-bar" style="width: 50%"></div>
+								</div>
+								<span class="progress-description">
+									50% Increase in 30 Days
+								</span>
+							</div>
+						</div>
+					</div>
+
+					<div class="col-md-3 col-sm-6 col-xs-12">
+							<div class="info-box bg-yellow">
+								<span class="info-box-icon"><i class="ion ion-ios-pricetag-outline"></i></span>
+								<div class="info-box-content">
+									<span class="info-box-text">Inventory</span>
+									<span class="info-box-number">5,200</span>
+									<div class="progress">
+										<div class="progress-bar" style="width: 50%"></div>
+									</div>
+									<span class="progress-description">
+										50% Increase in 30 Days
+									</span>
+								</div>
+							</div>
+						</div>
+
+						<div class="col-md-3 col-sm-6 col-xs-12">
+								<div class="info-box bg-yellow">
+									<span class="info-box-icon"><i class="ion ion-ios-pricetag-outline"></i></span>
+									<div class="info-box-content">
+										<span class="info-box-text">Inventory</span>
+										<span class="info-box-number">5,200</span>
+										<div class="progress">
+											<div class="progress-bar" style="width: 50%"></div>
+										</div>
+										<span class="progress-description">
+											50% Increase in 30 Days
+										</span>
+									</div>
+								</div>
+							</div>
+
+
+
+
 				
+
+
 				<table class="table table-hover table-striped">
 					<tr>
 						<th>Caixa</th>
