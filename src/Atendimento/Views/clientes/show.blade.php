1388f4b386c0cfc8d2b7bb1dc7d00a85499c47c7
Bruno Manzoli do Nascimento <manzoli2122@gmail.com>
2018-02-14 12:06:50 -0200

    30-01-2018

--- src/Atendimento/Views/clientes/show.blade.php
+++ src/Atendimento/Views/clientes/show.blade.php
@@ -14,57 +14,68 @@
 
 <div class="col-md-12">
     <div class="box box-success">
+        
+
         <div class="box-body" style="background:#ecf0f5">
+            
             <div class="alert alert-default alert-dismissible align-center invisivel" id="divAlerta">
                 <label>Excluído</label>
             </div>
+
+            <a href="{{route('clientes.atender', $model->id)}}" class="btn btn-success" title="Atender" remover-apos-excluir> 
+                    <i class="fa fa-money"></i> Atender
+                </a>                    
+                <button type="button" class="btn btn-danger" id='btnExcluir' remover-apos-excluir>
+                    <i class="fa fa-times"></i> Excluir
+                </button>
+                @permissao('clientes-editar')
+                    <a href="{{route('clientes.edit', $model->id)}}" class="btn btn-info" title="Editar" remover-apos-excluir> 
+                        <i class="fa fa-pencil"></i> Editar
+                    </a>
+                @endpermissao
+                <a class="btn btn-default" href="{{ URL::previous() }}"><i class="fa fa-reply"></i> Voltar</a>
+
+
             <div class="box-header">			
 				<h2>Atendimentos</h2>
         	</div>           		
-			<section class="row text-center dados">            
-                @forelse($model->atendimentos as $atendimento)
-                    <div class="col-12 col-sm-4 servicos" style="margin-bottom:10px; ">
-                        <div class="row">        
-                            <div class="col-md-12">
-                                <div class="box box-success">
-                                    <div class="box-header with-border">
-                                        <h3 class="box-title" style="width: 100%;"> 
-                                            {{ $atendimento->created_at->format('d/m/Y') }}
-                                            <a class="btn btn-warning pull-right btn-sm" data-toggle="modal" data-target="#{{$atendimento->id}}atendimentoModal" > 
-                                                Visualizar
-                                            </a>									
-                                        </h3>  								                                                       
-                                    </div>                        
-                                    <div class="box-body">                               
-                                        <div class="direct-chat-msg">                                        
-                                            <div class="direct-chat-info clearfix">                               
-                                                <span class="pull-left">Valor: </span>
-                                                <span class="pull-right badge bg-green"> R$ {{number_format($atendimento->valor, 2)}} </span>
+			
+        </div>
+        
+
+        <div class="box-footer align-right" style="background:#ecf0f5">
+                <section class="row text-center dados">            
+                        @forelse($model->atendimentos as $atendimento)
+                            <div class="col-12 col-sm-4 servicos" style="margin-bottom:10px; ">
+                                <div class="row">        
+                                    <div class="col-md-12">
+                                        <div class="box box-success">
+                                            <div class="box-header with-border">
+                                                <h3 class="box-title" style="width: 100%;"> 
+                                                    {{ $atendimento->created_at->format('d/m/Y') }}
+                                                    <a class="btn btn-warning pull-right btn-sm" data-toggle="modal" data-target="#{{$atendimento->id}}atendimentoModal" > 
+                                                        Visualizar
+                                                    </a>									
+                                                </h3>  								                                                       
+                                            </div>                        
+                                            <div class="box-body">                               
+                                                <div class="direct-chat-msg">                                        
+                                                    <div class="direct-chat-info clearfix">                               
+                                                        <span class="pull-left">Valor: </span>
+                                                        <span class="pull-right badge bg-green"> R$ {{number_format($atendimento->valor, 2)}} </span>
+                                                    </div>
+                                                </div>
                                             </div>
                                         </div>
-                                    </div>
+                                    </div>                    
                                 </div>
-                            </div>                    
-                        </div>
-                    </div>
-                @empty			
-                @endforelse        
-            </section>
+                            </div>
+                        @empty			
+                        @endforelse        
+                    </section>
         </div>
-        <div class="box-footer align-right" style="background:#ecf0f5">
-            <a href="{{route('clientes.atender', $model->id)}}" class="btn btn-success" title="Atender" remover-apos-excluir> 
-                <i class="fa fa-money"></i> Atender
-            </a>                    
-            <button type="button" class="btn btn-danger" id='btnExcluir' remover-apos-excluir>
-                <i class="fa fa-times"></i> Excluir
-            </button>
-            @permissao('clientes-editar')
-                <a href="{{route('clientes.edit', $model->id)}}" class="btn btn-info" title="Editar" remover-apos-excluir> 
-                    <i class="fa fa-pencil"></i> Editar
-                </a>
-            @endpermissao
-            <a class="btn btn-default" href="{{ URL::previous() }}"><i class="fa fa-reply"></i> Voltar</a>
-        </div>
+
+
     </div>
 </div>
 
